d85d606b6acf787ef360394adc1e27da3343ceab
Daiki Hirose <programer.sd131@gmail.com>
2020-10-04 14:57:15 +0900

    commit

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -1,21 +1,46 @@
 
-let firstName = 'John';
-let lastName = 'Smith';
-const yearOfBirth = 1990;
+// let firstName = 'John';
+// let lastName = 'Smith';
+// const yearOfBirth = 1990;
 
-function calcAge(year) {
-    return 2016 - year;
+// function calcAge(year) {
+//     return 2016 - year;
+// }
+
+// // ES5
+// console.log('This is ' + firstName + ' ' + lastName + '. He was born in ' + yearOfBirth + '. Today, he is ' + calcAge(yearOfBirth) + ' years old.');
+
+// // ES6
+// console.log(`This is ${firstName} ${lastName}. He was born in ${yearOfBirth}. Today, he is ${calcAge(yearOfBirth)} years old.`);
+
+
+// const n = `${firstName} ${lastName}`;
+// console.log(n.startsWith('j'));
+// console.log(n.endsWith('Sm'));
+// console.log(n.includes('oh'));
+// console.log(`${firstName} `.repeat(5));
+
+
+
+const years = [1990, 1997, 1982, 1937];
+
+const age = years.map(el => 2016 - el);
+console.log(age);
+
+
+const box = {
+    color: 'green',
+    position: 1,
+
+    clickMe: function() { //もしここで矢印を使ったら、グローバルなthisになってしまう。
+
+        document.querySelector('.green').addEventListener('click',
+        () => {
+            let str = 'This is box number' + this.position + this.color;
+            alert(str);
+        });
+    }
 }
-
-// ES5
-console.log('This is ' + firstName + ' ' + lastName + '. He was born in ' + yearOfBirth + '. Today, he is ' + calcAge(yearOfBirth) + ' years old.');
-
-// ES6
-console.log(`This is ${firstName} ${lastName}. He was born in ${yearOfBirth}. Today, he is ${calcAge(yearOfBirth)} years old.`);
+box.clickMe()
 
 
-const n = `${firstName} ${lastName}`;
-console.log(n.startsWith('j'));
-console.log(n.endsWith('Sm'));
-console.log(n.includes('oh'));
-console.log(`${firstName} `.repeat(5));
